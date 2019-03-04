a3696c7a8b774020403f0fd5566e0069a8d65d63
patrickvienneau <34248317+patrickvienneau@users.noreply.github.com>
2019-03-04 17:17:19 -0500

    Integrations API Client (#1)
    * Initial dump of api client
    
    * Dump

--- client.js
+++ client.js
@@ -1,4 +1,4 @@
-var VeemApi = require('veem_api')
+var ApiClient = require('./lib/utils/api-client')
 var Payment = require('./lib/resources/payment')
 var Invoice = require('./lib/resources/invoice')
 var Attachment = require('./lib/resources/attachment')
@@ -9,35 +9,25 @@ var Contact = require('./lib/resources/contact')
 var Customer = require('./lib/resources/customer')
 
 function Client(configuration) {
-  this.api = VeemApi
-
-  this.Metadata = new Metadata(this)
-  this.payment = new Payment(this)
-  this.invoice = new Invoice(this)
-  this.exchangeRate = new ExchangeRate(this)
-  this.authorization = new Authorization(this)
-  this.contact = new Contact(this)
-  this.customer = new Customer(this)
-
   this.setup(configuration)
-  this.generateToken()
+  this.authenticate(configuration.accessToken)
 }
 
 Client.prototype.setup = function (configuration) {
   this.configuration = configuration
+  this.apiClient = new ApiClient(configuration.environment)
+
+  this.metadata = new Metadata(this.apiClient)
+  this.payment = new Payment(this.apiClient)
+  this.invoice = new Invoice(this.apiClient)
+  this.exchangeRate = new ExchangeRate(this.apiClient)
+  this.authorization = new Authorization(this.apiClient)
+  this.contact = new Contact(this.apiClient)
+  this.customer = new Customer(this.apiClient)
 }
 
-Client.prototype.generateToken = function () {
-  var _this = this
-
-  var callback = function(error, data) {
-    _this.api.ApiClient.instance.authentications['OAuth2'].accessToken = data.access_token
-  }
-
-  var access = this.authorization.generate({
-    clientId: this.configuration.clientId,
-    clientSecret: this.configuration.clientSecret,
-  }, callback)
+Client.prototype.authenticate = function (accessToken) {
+  this.apiClient.accessToken = accessToken
 }
 
 module.exports = Client
--- node/app.js
+++ node/app.js
@@ -41,7 +41,7 @@ const QUOTE = {
 }
 
 const CONTACT = {
-  email: 'test+contact@domain.com',
+  email: `test+contact2@domain.com`,
   firstName: 'FName',
   lastName: 'LName',
   isoCountryCode: 'US',
@@ -53,50 +53,50 @@ const CONTACT = {
 var callback = function(error, data, response) {
   if (error) {
     console.warn('ERROR')
-    console.warn(error)
-    console.error(error.response.body.message)
+    // console.error(JSON.parse(error, null, 2))
+    console.warn(response.request)
     console.warn('ERROR')
   } else {
     console.log('API called successfully.')
-    console.log(data)
+    console.log(JSON.parse(data, null, 2))
   }
 }
 
 function main() {
   // var file = fs.readFileSync(__dirname+'/attachment.txt')
   var file = __dirname+'/attachment.txt'
-
-
-  // veemSDK.countryCurrency.get(null, callback)
-
-  // veemSDK.payment.list(callback)
-  // veemSDK.payment.get(54090, callback)
-  // veemSDK.payment.draft(PAYMENT, callback)
-  // veemSDK.payment.send(PAYMENT, callback)
-  // veemSDK.payment.sendById(54097, callback)
-  // veemSDK.payment.cancel(54098, callback)
-
-  // veemSDK.invoice.get(54090, callback)
-  // veemSDK.invoice.draft(INVOICE, callback)
-  // veemSDK.invoice.send(INVOICE, callback)
-  // veemSDK.invoice.sendById(36909, callback)
-  // veemSDK.invoice.cancel(36909, callback)
-
-  // veemSDK.contact.list(callback)
-  // veemSDK.contact.get(1459, callback)
-  // veemSDK.contact.create(CONTACT, callback)
-
-  veemSDK.customer.list('bitheads2@mailinator.com', callback)
-
-  // veemSDK.exchangeRate.quote(QUOTE, callback)
-
-  // veemSDK.attachment.upload(file, callback)
 }
 
+// const veemSDK = new VeemSDK({
+//   clientId: 'BizName-4be1d204',
+//   clientSecret: '732b5451-5265-4daa-8aea-335971603ef1',
+// })
 
 const veemSDK = new VeemSDK({
-  clientId: 'BizName-4be1d204',
-  clientSecret: '732b5451-5265-4daa-8aea-335971603ef1',
+  accessToken: '246ff312-f7ff-496f-bab2-38d132434ba7',
 })
 
-setTimeout(main, 2000)
+// veemSDK.metadata.getCountryCurrencyMap(callback)
+
+// veemSDK.payment.list(callback)
+// veemSDK.payment.get(54090, callback)
+// veemSDK.payment.draft(PAYMENT, callback)
+// veemSDK.payment.send(PAYMENT, callback)
+// veemSDK.payment.sendById(54133, callback)
+// veemSDK.payment.cancel(54135, callback)
+
+// veemSDK.invoice.get(36909, callback)
+// veemSDK.invoice.draft(INVOICE, callback)
+// veemSDK.invoice.send(INVOICE, callback)
+// veemSDK.invoice.sendById(36913, callback)
+// veemSDK.invoice.cancel(36913, callback)
+
+// veemSDK.contact.list(callback)
+// veemSDK.contact.get(1459, callback)
+// veemSDK.contact.create(CONTACT, callback)
+
+// veemSDK.customer.list('bitheads2@mailinator.com', callback)
+
+// veemSDK.exchangeRate.quote(QUOTE, callback)
+
+// veemSDK.attachment.upload(file, callback)
