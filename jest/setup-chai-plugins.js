c0e7ac9ec0a6d106c6a3a819eac99ebb68581ef4
patrickvienneau <34248317+patrickvienneau@users.noreply.github.com>
2019-03-18 20:46:11 -0400

    Implements Missing Tests for Models (#20)
    * Impleemnts missing tests for models
    
    * Installs chai-json-schema-ajv

--- jest/setup-chai-plugins.js
+++ jest/setup-chai-plugins.js
@@ -2,7 +2,9 @@ import chai from 'chai'
 import sinonChai from 'sinon-chai'
 import nockChai from 'chai-nock'
 import chaiJestDiff from 'chai-jest-diff'
+import chaiJsonSchema from 'chai-json-schema-ajv'
 
 chai.use(sinonChai)
 chai.use(nockChai)
 chai.use(chaiJestDiff())
+chai.use(chaiJsonSchema)
