826162d73c796a135f429bb968c9358f68d45a7e
patrickvienneau <34248317+patrickvienneau@users.noreply.github.com>
2019-03-28 13:15:25 -0400

    Implements Internal Plugin to Assert API Response Bodies (#27)
    * Implements attachments for invoices
    
    * Dump
    
    * Dump
    
    * Implements base assertions for response body

--- jest/setup-chai-plugins.js
+++ jest/setup-chai-plugins.js
@@ -3,8 +3,10 @@ import sinonChai from 'sinon-chai'
 import nockChai from 'chai-nock'
 import chaiJestDiff from 'chai-jest-diff'
 import chaiJsonSchema from 'chai-json-schema-ajv'
+import chaiRequest from './plugins/chai-request'
 
 chai.use(sinonChai)
 chai.use(nockChai)
 chai.use(chaiJestDiff())
 chai.use(chaiJsonSchema)
+chai.use(chaiRequest)
